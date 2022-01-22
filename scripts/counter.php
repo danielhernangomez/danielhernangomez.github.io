ac04b9b4b07cf63fea4b6f89af562884bb62169c
danielhernangomez <89264491+danielhernangomez@users.noreply.github.com>
2022-01-22 12:49:15 +0200

    Delete counter.php
    erased counter

--- scripts/counter.php
+++
@@ -1,6 +0,0 @@
-<?php
-	function updateCounter($name) {
-        $count = file_get_contents("stats/".$name.".txt");
-        file_put_contents("stats/".$name.".txt", $count + 1); 
-                } 
-?>
