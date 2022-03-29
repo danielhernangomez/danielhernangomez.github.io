71145b84e3d8c9b6776e9f1a7a1b960925181fa4
Dani <daniel.hernangomez@gmail.com>
2022-03-30 00:34:49 +0300

    updated publication list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,11 +10,11 @@ function formatDate(fullDate) {
 
 
 function showFunction(s) {
-        				  var x = document.getElementById(s);
-        				  if (x.style.display == 'none') {
-                		    x.style.display = 'block';
-                		    } else {
-                			x.style.display = 'none';
-                			}
-                		  }
+                          var x = document.getElementById(s);
+                              if (x.style.display == 'none') {
+                                x.style.display = 'block';
+                              } else {
+                                x.style.display = 'none';
+                            }
+                          }
 
